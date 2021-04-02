06848e14b67daa6336037044b6a5feee5fffde31
Eric Ihle <zekeotronic@gmail.com>
2021-04-02 02:53:26 -0400

    adds cookie shake

--- resources/js/main.js
+++ resources/js/main.js
@@ -5,7 +5,11 @@ const message = document.querySelector('.message');
 
 function randomMessage() {
   let rand = Math.floor(Math.random() * messages.length);
+  cookie.setAttribute('id', 'shake-lr');
   message.innerHTML = messages[rand];
+  setTimeout(() => {
+    cookie.setAttribute('id', 'shake');
+  },500)
 };
 
 cookie.addEventListener('click', randomMessage)
